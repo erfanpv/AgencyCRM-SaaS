0a6eda814415b1da769fd8daf7864413e97b74c0
erfanpv <erfanpv786@gmail.com>
2024-12-18 10:03:01 +0530

    chore: update middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,55 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/site","/agency/sign-up(.*)","/agency/sign-in(.*)"]);
+// Define public routes
+const isPublicRoute = createRouteMatcher(["/site", "/api/uploadthing"]);
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect(); // Protect all other routes
+export default clerkMiddleware((auth, req) => {
+  const url = req.nextUrl;
+  const searchParams = url.searchParams.toString();
+  const pathWithSearchParams = `${url.pathname}${
+    searchParams ? `?${searchParams}  ` : ""
+  }`;
+
+  const hostname = req.headers.get("host") || "";
+
+  // Allow public routes
+  if (isPublicRoute(req)) {
+    return NextResponse.next();
   }
+
+  // Subdomain logic
+  const customSubDomain = hostname
+    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    .filter(Boolean)[0];
+
+  if (customSubDomain) {
+    return NextResponse.rewrite(
+      new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+    );
+  }
+
+  // Redirect sign-in and sign-up routes
+  if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
+    return NextResponse.redirect(new URL("/agency/sign-in", req.url));
+  }
+
+  // Default rewrite for '/' or public pages
+  if (
+    url.pathname === "/" ||
+    (url.pathname === "/site" && hostname === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.rewrite(new URL("/site", req.url));
+  }
+
+  return NextResponse.next();
 });
 
+// Middleware matcher configuration
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    "/((?!.+\\.[\\w]+$|_next/static|_next/image|favicon.ico).*)",
+    "/",
     "/(api|trpc)(.*)",
   ],
 };
