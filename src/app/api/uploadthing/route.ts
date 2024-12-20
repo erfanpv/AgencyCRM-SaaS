9fae5297e095aa643b99ff90d4bb43e039f90d13
erfanpv <erfanpv786@gmail.com>
2024-12-20 15:53:07 +0530

    feat(agency): add agency page and upload API routes

---
+++ src/app/api/uploadthing/route.ts
@@ -0,0 +1,5 @@
+import { createRouteHandler } from "uploadthing/next";
+import { fileRouter } from "./core";
+
+// Export routes for Next App Router
+export const { GET, POST } = createRouteHandler({ router: fileRouter });
