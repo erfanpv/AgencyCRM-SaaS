ff80a80842d9b92e608e5db157b16fe52a984e2c
erfanpv <erfanpv786@gmail.com>
2024-12-21 20:25:57 +0530

    refactor: enhance agency page component for better data handling

--- src/app/(main)/agency/[agencyId]/page.tsx
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const AgencyIdPage = ({ params }: { params: { agencyId: string } }) => {
+  return <div>{params.agencyId}</div>;
+};
 
-export default page
+export default AgencyIdPage;
