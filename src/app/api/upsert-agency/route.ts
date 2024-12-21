4368cb4802a0b667d617598575b9a447e8e7741e
erfanpv <erfanpv786@gmail.com>
2024-12-21 20:24:32 +0530

    feat: add API route for upserting agency data

---
+++ src/app/api/upsert-agency/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const { agency } = await request.json(); 
+    if (!agency.companyEmail) {
+      return NextResponse.json({ error: "Agency email is required." }, { status: 400 });
+    }
+
+    const agencyDetails = await db.agency.upsert({
+      where: { id: agency.id },
+      update: agency,
+      create: {
+        users: {
+          connect: { email: agency.companyEmail },
+        },
+        ...agency,
+        SidebarOption: {
+          create: [
+            { name: "Dashboard", icon: "category", link: `/agency/${agency.id}` },
+            { name: "Launchpad", icon: "clipboardIcon", link: `/agency/${agency.id}/launchpad` },
+            { name: "Billing", icon: "payment", link: `/agency/${agency.id}/billing` },
+            { name: "Settings", icon: "settings", link: `/agency/${agency.id}/settings` },
+            { name: "Sub Accounts", icon: "person", link: `/agency/${agency.id}/all-subaccounts` },
+            { name: "Team", icon: "shield", link: `/agency/${agency.id}/team` },
+          ],
+        },
+      },
+    });
+
+    return NextResponse.json(agencyDetails, { status: 200 });
+  } catch (error) {
+    console.error("Error in upsertAgency:", error);
+    return NextResponse.json({ error: "Internal server error." }, { status: 500 });
+  }
+}
