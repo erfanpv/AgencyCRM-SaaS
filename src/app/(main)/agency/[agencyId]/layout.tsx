6baac56f67497417762b6f011c5596718be7b21d
erfanpv <erfanpv786@gmail.com>
2024-12-26 17:30:34 +0530

    feat: add layout for agency page and unauthorized access page

---
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+import { Role } from "@prisma/client";
+import Sidebar from "@/components/navigation/Sidebar";
+import BlurPage from "@/components/global/BlurPage";
+import { verifyAndAcceptInvitation } from "@/queries/invitation";
+import { getNotification } from "@/queries/notification";
+
+interface AgencyIdLayoutProps extends React.PropsWithChildren {
+  params: {
+    agencyId: string | undefined;
+  };
+}
+
+const AgencyIdLayout: React.FC<AgencyIdLayoutProps> = async ({
+  params,
+  children,
+}) => {
+  const user = await currentUser();
+  const agencyId = await verifyAndAcceptInvitation();
+
+  if (!user) redirect("/");
+  if (!agencyId || !params.agencyId) redirect("/agency");
+
+  if (
+    user.privateMetadata.role !== Role.AGENCY_OWNER &&
+    user.privateMetadata.role !== Role.AGENCY_ADMIN
+  ) {
+    redirect("/agency/unauthorized");
+  }
+
+  const notifications = await getNotification(agencyId);
+
+  return (
+    <div className="h-screen overflow-hidden">
+      <Sidebar id={params.agencyId} type="agency" />
+      <div className="md:pl-[300px]">
+        {/* <InfoBar
+          notifications={notifications}F
+          subAccountId={user.id}
+          role={user.privateMetadata.role}
+        /> */}
+        <div className="relative">
+          <BlurPage>{children}</BlurPage>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AgencyIdLayout;
---
+++ src/app/(main)/agency/unauthorized/page.tsx
@@ -0,0 +1,10 @@
+import Unauthorized from "@/components/global/Unauthorized";
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return <Unauthorized />;
+};
+
+export default Page;
