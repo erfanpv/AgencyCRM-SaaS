3bd08306436d85e782dec4fcbf2c060fada6bc08
erfanpv <erfanpv786@gmail.com>
2025-01-05 21:43:41 +0530

    feat: add user data validator; update agency details validator and type definitions

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,5 +1,17 @@
-import type { Notification, User } from "@prisma/client";
+import { getAuthUserDetails } from "@/queries/auth";
+import { getUserWithPermissionsAndSubAccount } from "@/queries/permission";
+import type { Notification, Prisma, User } from "@prisma/client";
 
 export type NotificationsWithUser =
   | ({ user: User } & Notification)[]
   | undefined;
+
+export type AuthUserWithAgencySidebarOptionsAndSubAccounts =
+  Prisma.PromiseReturnType<typeof getAuthUserDetails>;
+
+export type UsersWithAgencySubAccountPermissionsSidebarOptions =
+  Prisma.PromiseReturnType<typeof getAuthUserDetails>;
+
+export type UserWithPermissionsAndSubAccounts = Prisma.PromiseReturnType<
+  typeof getUserWithPermissionsAndSubAccount
+>;
--- src/lib/validators/agency-details.ts
+++ src/lib/validators/agency-details.ts
@@ -1,24 +1,25 @@
-import { z } from "zod";
+  import { z } from "zod";
 
-export const AgencyDetailsValidator = z.object({
-  name: z
-    .string()
-    .min(2, { message: "Agency name must be at least 2 characters." }),
-  companyEmail: z.string().email({ message: "Invalid email address." }),
-  companyPhone: z.string().min(5, {
-    message: "Phone number must be at least 5 characters.",
-  }),
-  whiteLabel: z.boolean(),
-  address: z.string().min(1, {
-    message: "Address must be at least 1 character.",
-  }),
-  city: z.string().min(1, {
-    message: "City must be at least 1 character.",
-  }),
-  zipCode: z.string().min(1),
-  state: z.string().min(1),
-  country: z.string().min(1),
-  agencyLogo: z.string().url({ message: "Invalid URL." }),
-});
+  export const AgencyDetailsValidator = z.object({
+    name: z
+      .string()
+      .min(2, { message: "Agency name must be at least 2 characters." }),
+    companyEmail: z.string().email({ message: "Invalid email address." }),
+    companyPhone: z.string().min(5, {
+      message: "Phone number must be at least 5 characters.",
+    }),
+    whiteLabel: z.boolean(),
+    address: z.string().min(1, {
+      message: "Address must be at least 1 character.",
+    }),
+    city: z.string().min(1, {
+      message: "City must be at least 1 character.",
+    }),
+    zipCode: z.string().min(1),
+    state: z.string().min(1),
+    country: z.string().min(1),
+    agencyLogo: z.string().url({ message: "Invalid URL." }),
+    goal: z.number().min(1, "Goal must be a positive number"), 
+  });
 
-export type AgencyDetailsSchema = z.infer<typeof AgencyDetailsValidator>;
+  export type AgencyDetailsSchema = z.infer<typeof AgencyDetailsValidator>;
