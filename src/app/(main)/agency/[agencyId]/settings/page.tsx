b82952c8d3836485d3061f5ee331c872c5f885c2
erfanpv <erfanpv786@gmail.com>
2025-01-05 21:40:55 +0530

    refactor: improve agency page and settings page functionality

--- src/app/(main)/agency/[agencyId]/settings/page.tsx
+++ src/app/(main)/agency/[agencyId]/settings/page.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { redirect } from "next/navigation";
 
-const SettingsPage = () => {
-  return <div>Settings Page</div>;
+import { getAuthUser } from "@/queries/auth";
+import { getAgencyDetails } from "@/queries/agency";
+
+import AgencyDetails from "@/components/forms/AgencyDetails";
+import UserDetailsForm from "@/components/forms/UserDetails";
+import { currentUser } from "@clerk/nextjs/server";
+
+interface AgencySettingsPageProps {
+  params: {
+    agencyId: string | undefined;
+  };
+}
+
+const AgencySettingsPage: React.FC<AgencySettingsPageProps> = async ({
+  params,
+}) => {
+  const { agencyId } = params;
+  const authUser = await currentUser();
+
+  if (!authUser) redirect("/agency/sign-in");
+  if (!agencyId) redirect("/agency/unauthorized");
+
+  const userDetails = await getAuthUser(authUser.emailAddresses[0].emailAddress);
+
+  if (!userDetails) redirect("/agency/sign-in");
+
+  const agencyDetails = await getAgencyDetails(agencyId);
+
+  if (!agencyDetails) redirect("/agency/unauthorized");
+
+  const subAccounts = agencyDetails.SubAccount;
+
+  return (
+    <div className="flex flex-col gap-4 max-w-4xl w-full mx-auto">
+      <AgencyDetails data={agencyDetails} />
+      <UserDetailsForm
+        type="agency"
+        id={agencyId}
+        subAccounts={subAccounts}
+        userData={userDetails}
+      />
+    </div>
+  );
 };
 
-export default SettingsPage;
+export default AgencySettingsPage;
